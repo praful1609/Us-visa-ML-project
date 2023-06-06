24796eb7c93bb9a3a097553e563c29000d3a0a5f
Praful Bhojane <pbhojane1609@gmail.com>
2023-06-06 10:57:05 +0530

    config_entity file updated

--- visa/entity/config_entity.py
+++ visa/entity/config_entity.py
@@ -1,3 +1,10 @@
 from collections import namedtuple #tuple is immutable and namedtuple is mutable
 
-DataIngestionConfig
+DataIngestionConfig: namedtuple("DataIngestionConfig", 
+                                ["dataset_download_url",
+                                 "raw_data_dir",
+                                 "ingested_train_dir",
+                                 "ingested_test_dir"])
+
+DataValidationConfig: namedtuple("DataValidationConfig", 
+                                 ["scheme_file_dir"])
