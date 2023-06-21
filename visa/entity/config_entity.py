54ad6f0cc32b6e62c8a5880feaafe6cda37b9857
Praful Bhojane <pbhojane1609@gmail.com>
2023-06-21 23:50:27 +0530

    config_entity folder updated

--- visa/entity/config_entity.py
+++ visa/entity/config_entity.py
@@ -10,7 +10,9 @@ DataValidationConfig = namedtuple("DataValidationConfig", ["schema_file_path"])
 
 DataTransformationConfig = namedtuple("DataTransformationConfig", ["transformed_train_dir",
                                                                    "transformed_test_dir",
-                                                                   "preprocessed_object_file_path"])
+                                                                  "preprocessed_object_file_path"])
+
+ModelTrainerConfig = namedtuple("ModelTrainerConfig", ["trained_model_file_path", "base_accuracy", "model_config_file_path"])
 
 
 TrainingPipelineConfig = namedtuple("TrainingPipelineConfig", ["artifact_dir"])
