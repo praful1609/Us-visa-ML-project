d19d3592f8bd109ee672d42bfc2e4ce02f5bf63d
Praful Bhojane <pbhojane1609@gmail.com>
2023-06-24 12:40:34 +0530

    constant.py file updated

--- visa/constant/__init__.py
+++ visa/constant/__init__.py
@@ -65,3 +65,16 @@ MODEL_TRAINER_TRAINED_MODEL_FILE_NAME_KEY = "model_file_name"
 MODEL_TRAINER_BASED_ACCURACY_KEY = "base_accuracy"
 MODEL_TRAINER_MODEL_CONFIG_DIR_KEY = "model_config_dir"
 MODEL_TRAINER_MODEL_CONFIG_FILE_NAME_KEY = "model_config_file_name"
+
+#Model Evaluation Variable
+MODEL_EVALUATION_CONFIG_KEY = "model_evaluation_config"
+MODEL_EVALUATION_FILE_NAME_KEY = "model_evaluation_file_name"
+MODEL_EVALUATION_ARTIFACT_DIR = "model_evaluation"
+
+BEST_MODEL_KEY = "best_model"
+HISTORY_KEY = "history"
+MODEL_PATH_KEY = "model_path"
+
+#Model Pusher related variable
+MODEL_PUSHER_CONFIG_KEY = 'model_pusher_config'
+MODEL_PUSHER_MODEL_EXPORT_DIR_KEY = 'model_export_dir'
